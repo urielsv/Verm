89fc459bc2ea06f9238f250c761c3e452a75dfb8
Uriel Sosa Vazquez <usosavazquez@itba.edu.ar>
2025-06-21 22:02:46 -0300

    Fix ubuntu build to work in *nix

--- script/ubuntu/build.sh
+++ script/ubuntu/build.sh
@@ -5,10 +5,10 @@ set -euxo pipefail
 BASE_PATH="$(dirname "$0")/../.."
 cd "$BASE_PATH"
 
-rm --force --recursive "$BASE_PATH/build"
-rm --force "$BASE_PATH/src/main/c/frontend/lexical-analysis/FlexScanner.c"
-rm --force "$BASE_PATH/src/main/c/frontend/syntactic-analysis/BisonParser.c"
-rm --force "$BASE_PATH/src/main/c/frontend/syntactic-analysis/BisonParser.h"
+rm -rf "$BASE_PATH/build"
+rm -f "$BASE_PATH/src/main/c/frontend/lexical-analysis/FlexScanner.c"
+rm -f "$BASE_PATH/src/main/c/frontend/syntactic-analysis/BisonParser.c"
+rm -f "$BASE_PATH/src/main/c/frontend/syntactic-analysis/BisonParser.h"
 
 cmake -S . -B build
 cd build
